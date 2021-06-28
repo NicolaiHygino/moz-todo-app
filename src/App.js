4d15fa6e7dbc9c98fc5c3e75aba2cf3c6d59cb57
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-06-28 13:51:02 -0300

    Now is possible to add tasks to the list and have unique IDs using nanoid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
-
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks.map(task => (
       <Todo 
         id={task.id}
         name={task.name} 
@@ -13,11 +20,11 @@ function App(props) {
         key={task.id}
       />
     ));
-  
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
