58e66fbb38ce893b4fd569856dc1617a0c7e21fe
NicolaiHygino <nicolaihygino2000@gmail.com>
2021-06-28 13:58:57 -0300

    Making the task counter work

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ function App(props) {
       />
     ));
 
+  const tasksNoun = taskList.length === 1 ? 'task' : 'tasks';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`; 
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -30,9 +33,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
